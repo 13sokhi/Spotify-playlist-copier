21af297d7f49f21384e9d5d1d3bcb5df0522340c
manparvegithub <manparve@sheridancollege.ca>
2024-07-12 22:28:30 -0400

    config file updated

--- config.py
+++ config.py
@@ -4,9 +4,11 @@ youtube_api_key = 'AIzaSyAyDlAEr_Q1kPLb8hHtX20LomdzyokrQXw'
 
 # spotify credentials
 spotify_client_id = '4f0a58fe202c4bb19a37a8e4a6445970'
-spotofy_client_secret = '853cbdf5695c43429e7087783d1d442f'
+spotify_client_secret = '853cbdf5695c43429e7087783d1d442f'
+spotify_redirect_uri = 'https://localhost:3000'
 
 # Spotify request endpoints
 user_profile_endpoint = 'https://api.spotify.com/v1/me'
 base_authorization_url = 'https://accounts.spotify.com/authorize'
 token_url = 'https://accounts.spotify.com/api/token'
+base_youtube_search_url = 'https://www.youtube.com/results?search_query='
