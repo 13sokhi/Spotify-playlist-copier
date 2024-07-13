1175f7966cee597a617a764075d28b6cfa30797f
manparvegithub <manparve@sheridancollege.ca>
2024-07-12 22:28:14 -0400

    minor changes and basic exceptional handling added

--- SpotifyDataAccess.py
+++ SpotifyDataAccess.py
@@ -1,4 +1,3 @@
-from SpotifyArtist import SpotifyArtist
 from SpotifyTrack import SpotifyTrack
 from SpotifyPlaylist import SpotifyPlaylist
 from SpotifyToken import SpotifyToken
@@ -26,20 +25,12 @@ def get_playlist_items_json(playlist: SpotifyPlaylist, token: SpotifyToken) -> s
     response = requests.get(url=playlist_item_endpoint, headers=header).json()
     return response
 
-def get_artists_json(track: SpotifyTrack, token: SpotifyToken) -> list[str]:
-    all_artist_json: list[str] = list([])
-    artist_ids = track.get_artist_ids()
-    access_token = token.get_access_token()
-
-    for id in artist_ids:
-        artist_endpoint = f'https://api.spotify.com/v1/artists/{id}'
-        header = {"Authorization": f"Bearer {access_token}"}
-        response = requests.get(url=artist_endpoint, headers=header).json()
-        all_artist_json.append(response)
-
 def get_spotify_playlists(playlist_json: str) -> list[SpotifyPlaylist]:
     spotify_playlists: list[SpotifyPlaylist] = list([])
     playlists = playlist_json['items']
+
+    if len(playlists) == 0:
+        raise Exception('You have NO Spotify Playlists!')
 
     for playlist in playlists:
         id: str = playlist['id']
@@ -51,19 +42,15 @@ def get_spotify_playlists(playlist_json: str) -> list[SpotifyPlaylist]:
 def get_spotify_tracks(playlist_item_json: str) -> list[SpotifyTrack]:
     spotify_tracks: list[SpotifyTrack] = list([])
     playlist_items = playlist_item_json['items']
+
+    if len(playlist_items) == 0:
+        raise Exception('Playlist has NO Tracks!')
+
     for playlist_item in playlist_items:
         id: str = playlist_item['track']['id']
         name: str = playlist_item['track']['name']
-        artist_ids: list[str] = list([])
+        artists: list[str] = list([])
         for artist in playlist_items['track']['artists']:
-            artist_ids.append(artist)['id']
-        spotify_tracks.append(SpotifyTrack(id=id, name=name, artist_ids=artist_ids))
+            artists.append(artist)['name']
+        spotify_tracks.append(SpotifyTrack(id=id, name=name, artists=artists))
     return spotify_tracks
-
-def get_spotify_artists(all_artists_json: list[str]) -> list[SpotifyArtist]:
-    spotify_artists: list[SpotifyArtist] = list([])
-    for artist_json in all_artists_json:
-        id: str = artist_json['id']
-        name: str = artist_json['name']
-        spotify_artists.append(SpotifyArtist(id=id, name=name))
-    return spotify_artists
