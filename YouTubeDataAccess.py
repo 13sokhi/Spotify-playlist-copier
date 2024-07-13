8fd3afa89b1e1864afeb7abec3f6e68cdfe9760a
manparvegithub <manparve@sheridancollege.ca>
2024-07-12 22:26:06 -0400

    method to check if entered channel id exist or not added

--- YouTubeDataAccess.py
+++ YouTubeDataAccess.py
@@ -1,5 +1,3 @@
-from config import youtube_client_secret_json_path
-
 from YouTubePlaylistItem import YouTubePlaylistItem
 from YouTubePlaylist import YouTubePlaylist
 
@@ -8,7 +6,33 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-def get_playlists_json(channel_id: str, oauth_json_path: str) -> list[str]:
+def verify_youtube_channel(channel_id: str, oauth_json_path: str) -> None:
+    scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = oauth_json_path
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_local_server()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        id="UC_x5XG1OV2P6uZZ5FSM9Ttw"
+    )
+    response = request.execute()
+    if response['pageInfo']['totalResults'] == 0:
+        raise Exception('Channel ID is invalid!')
+
+def get_playlists_json(channel_id: str, oauth_json_path: str) -> str:
     scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -32,18 +56,21 @@ def get_playlists_json(channel_id: str, oauth_json_path: str) -> list[str]:
         maxResults=100
     )
     response = request.execute()
-    return list([response])
+    return response
 
 
 def get_youtube_playlists(playlist_json: str) -> list[YouTubePlaylist]:
     youtube_playlists: list[YouTubePlaylist] = list([])
     playlists = playlist_json['items']
 
+    if len(playlists) == 0:
+        raise Exception('No Playlists found!')
+
     for playlist in playlists:
         playlist_id = playlist['id']
         playlist_title = playlist['snippet']['title']
         playlist_size = playlist['contentDetails']['itemCount']
-        youtube_playlists.append(YouTubePlaylist(playlist_id, playlist_title, playlist_size))
+        youtube_playlists.append(YouTubePlaylist(playlist_id, playlist_title, int(playlist_size)))
 
     return youtube_playlists
 
@@ -79,10 +106,14 @@ def get_youtube_playlist_items(playlist_items_json: str) -> list[YouTubePlaylist
     youtube_playlist_items: list[YouTubePlaylistItem] = list([])
     items = playlist_items_json['items']
 
+    if len(items) == 0:
+        raise Exception('Playlist has NO Videos!')
+
     for item in items:
         id = item['id']
+        title = item['snippet']['title']
         playlist_id = item['playlistId']
         video_id = item['resourceId']['videoId']
-        youtube_playlist_items.append(YouTubePlaylistItem(id, playlist_id, video_id))
+        youtube_playlist_items.append(YouTubePlaylistItem(id, title, playlist_id, video_id))
 
     return youtube_playlist_items
